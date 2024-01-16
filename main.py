7f615aba14083103eb919312b9c5da15a8ae02f3
TonyGrif <TonyGriffin2000@gmail.com>
2024-01-16 17:02:26 -0500

    accept seed URI and debug flag as arguments

--- main.py
+++ main.py
@@ -2,9 +2,29 @@
 
 """Main module for the URI Gatherer."""
 
+import argparse
+import logging
+
 
 def main():
-    print("Hello, World!")
+    """Main driver of the URI Gatherer script."""
+    parser = argparse.ArgumentParser(
+        description="Gather unique URIs from links found in the seed URI."
+    )
+
+    parser.add_argument("seed_uri", help="URI to scan for other unique URIs.")
+
+    parser.add_argument(
+        "-d",
+        "--debug",
+        action="store_true",
+        help="Output all program debug logs to console.",
+    )
+
+    args = parser.parse_args()
+
+    if args.debug is True:
+        logging.basicConfig(level=logging.DEBUG)
 
 
 if __name__ == "__main__":
