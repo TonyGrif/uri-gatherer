da483f42a122dbc6653d9339aaab406ea09e567a
TonyGrif <TonyGriffin2000@gmail.com>
2024-01-16 20:51:00 -0500

    pass args and set up pseudocode

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,7 +1,16 @@
 import pytest
 
-from src.utils import func_import
+from src.utils import extract_links
 
 
-def test_import():
-    assert True
+@pytest.fixture
+def seed_uri():
+    return "https://weiglemc.github.io/"
+
+
+class TestUtils:
+    def test_get_uris(self):
+        pass
+
+    def test_extract_links(self):
+        pass
