2669ad400c7da3b08454f93105ff2ffa31dbc6c6
TonyGrif <TonyGriffin2000@gmail.com>
2024-01-17 11:52:14 -0500

    make initial request

--- src/utils.py
+++ src/utils.py
@@ -6,10 +6,16 @@ making HTTP requests and parsing the HTML responses.
 
 from typing import List
 
+import requests
+
+# TODO: Change to parameter and CLI optional argument
+TIMEOUT_TIME = 5
+
 
 def get_unique_uris(seed_uri: str, total_uri: int) -> List[str]:
     """Get the unique URIs from the seed URI and recursively search them
-    for more unique URIs.
+    for more unique URIs until the upper bound has been hit or no more can
+    be found.
 
     Parameters:
         seed_uri (str): The starting URI to make requests on.
@@ -18,25 +24,26 @@ def get_unique_uris(seed_uri: str, total_uri: int) -> List[str]:
     Returns:
         A list containing strings of unique URIs.
     """
-    # Make request on seed
-    # Parse for links
+    response = requests.get(seed_uri, timeout=TIMEOUT_TIME)
+    # Ensure valid content type; If not ...
+    links = extract_links(response)
     # Check if ready to return (collection >= total)
     # If so return as list/dictionary
     # Else, pick random URI
     # Call function again with new seed
 
-    return []
+    return links
 
 
-def extract_links() -> List[str]:
+def extract_links(response: requests.Response) -> List[str]:
     """Extract the links from an HTTP request.
 
     Parameters:
+        response (Response): A valid response object.
 
     Returns:
         A list containing strings of unique URIs.
     """
-    # Assume valid response
     # For each link
     # Grab content/type header
     # Disregard link if not text/html
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,16 +1,22 @@
 import pytest
+import requests
 
-from src.utils import extract_links
+from src.utils import get_unique_uris, extract_links
 
 
 @pytest.fixture
 def seed_uri():
     return "https://weiglemc.github.io/"
 
+@pytest.fixture
+def seed_request(seed_uri):
+    return requests.get(seed_uri, timeout=5)
+
 
 class TestUtils:
-    def test_get_uris(self):
-        pass
+    def test_get_uris(self, seed_uri):
+        uris = get_unique_uris(seed_uri, 10)
+        assert len(uris) == 10
 
-    def test_extract_links(self):
-        pass
+    def test_extract_links(self, seed_request):
+        assert seed_request.status_code == 200
